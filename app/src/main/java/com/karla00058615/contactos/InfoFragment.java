02cd0cfc17389fc5fa9730af63adb831aebb692b
KarlaLopez96}git config --global user.name KarlaLopez96 <00058615@uca.edu.sv>
2018-05-03 17:19:20 -0600

    "Funcionalidad del botón CALL, petición de permisos para llamar, creación de fragmento Edit y cambios menores al layout_info"

--- app/src/main/java/com/karla00058615/contactos/InfoFragment.java
+++ app/src/main/java/com/karla00058615/contactos/InfoFragment.java
@@ -1,10 +1,15 @@
 package com.karla00058615.contactos;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +26,7 @@ import android.widget.Toast;
  */
 public class InfoFragment extends Fragment {
 
+    private final int MY_PERMISSIONS_REQUEST_CALL = 123;
     private OnFragmentInteractionListener mListener;
     private TextView nombre,id,direccion,telefono,email;
     private Button buttonShare,buttonCall;
@@ -74,13 +80,54 @@ public class InfoFragment extends Fragment {
         buttonCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse("tel:"+bundle.getString("telefono")));
-                startActivity(intent);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(getContext(),
+                            Manifest.permission.CALL_PHONE)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        ActivityCompat.requestPermissions(getActivity(),
+                                new String[]{Manifest.permission.CALL_PHONE},
+                                MY_PERMISSIONS_REQUEST_CALL);
+
+                    } else {
+                        Call();
+                    }
+                }
             }
         });
 
         return view;
+    }
+
+    public void Call(){
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:"+bundle.getString("telefono")));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CALL: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Call();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
